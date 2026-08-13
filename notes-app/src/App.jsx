8450307942cb569f4f44834b1d854452680e96c9
atulgodbole <143529911+atul2188@users.noreply.github.com>
2026-08-13 09:36:06 +0530

    Local storage with useEffect

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import noteIcon from "./assets/note.png";
 import NoteForm from "./components/NoteForm";
 import NoteList from "./components/NoteList";
 
 const App = () => {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(() => {
+    const notes = JSON.parse(localStorage.getItem("notes"));
+    return notes || [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
 
   const deleteNote = (id) => {
     const confirmDelete = window.confirm(
